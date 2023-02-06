2067d454817d845bd688907f466c50f18d15b842
Matthieu BOHEAS <matthieu.boheas@digital4better.com>
2023-02-06 17:51:40 +0100

    update docker xbuild script

--- docker-build.sh
+++ docker-build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-# Setup:
-# export DOCKER_CLI_EXPERIMENTAL=enabled
-# docker run --rm --privileged docker/binfmt:a7996909642ee92942dcd6cff44b9b95f08dad64
+# Setup (host with qemu installed)
+# docker run --privileged --rm tonistiigi/binfmt --install all
 # docker buildx create --use --name multi-arch-builder
-# docker buildx inspect --bootstrap # check architectures
+# Check architectures
+# docker buildx inspect --bootstrap
 [[ -z $1 ]] && echo "Missing version. (like ./build-docker.sh 1.0.0)" && exit
 echo "Building(version: $1)"
 sleep 2
-docker buildx build --platform=linux/amd64,linux/arm64,linux/arm/v7 -t kelgors/gemweb:$1 .
+docker buildx build --platform=linux/amd64,linux/arm64,linux/arm/v7 -t kelgors/gemweb:$1 --push .
